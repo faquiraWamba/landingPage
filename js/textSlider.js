9452143d46f56196850a0bb3adf286483c44fad6
Faquira Wamba <faquirawamba15@gmail.com>
2022-07-29 11:36:13 +0100

    complete landing page

--- js/textSlider.js
+++ js/textSlider.js
@@ -11,14 +11,14 @@ firstTextSlid.addEventListener("click", (e)=> {
     
     e.preventDefault()
     if(secondTextSlid.classList.contains("checked")){
-        document.getElementById("textSlidWrap").style.transform = `translateX(${index + 100}vw)`
+        document.getElementById("textSlidWrap").style.transform = `translateX(${index + 98.7}vw)`
         document.getElementById("textSlidWrap").style.transition = "0.5s"
-        changeIndex(index + 100)
+        changeIndex(index + 98.7)
     }
     if(thirdTextSlid.classList.contains("checked")){
-        document.getElementById("textSlidWrap").style.transform = `translateX(${index + 200}vw)`
+        document.getElementById("textSlidWrap").style.transform = `translateX(${index + 197.4}vw)`
         document.getElementById("textSlidWrap").style.transition = "0.5s"
-        changeIndex(index + 200)
+        changeIndex(index + 197.4)
     }
     firstTextSlid.classList.add("checked")
     secondTextSlid.classList.remove("checked")
@@ -28,15 +28,15 @@ firstTextSlid.addEventListener("click", (e)=> {
 secondTextSlid.addEventListener("click", (e)=> {
     e.preventDefault()
     if(firstTextSlid.classList.contains("checked")){
-        document.getElementById("textSlidWrap").style.transform = `translateX(${index-100}vw)`
+        document.getElementById("textSlidWrap").style.transform = `translateX(${index-98.7}vw)`
         document.getElementById("textSlidWrap").style.transition = "0.5s"
-        changeIndex(index-100);
+        changeIndex(index-98.7);
         console.log(index)
     }
     if(thirdTextSlid.classList.contains("checked")){
-        document.getElementById("textSlidWrap").style.transform = `translateX(${index+100}vw)`
+        document.getElementById("textSlidWrap").style.transform = `translateX(${index+98.7}vw)`
         document.getElementById("textSlidWrap").style.transition = "0.5s"
-        changeIndex(index+100);
+        changeIndex(index+98.7);
         console.log(index)
     }
     secondTextSlid.classList.add("checked")
@@ -47,15 +47,15 @@ thirdTextSlid.addEventListener("click", (e)=> {
     e.preventDefault()
     if(firstTextSlid.classList.contains("checked")){
         changeIndex(0)
-        document.getElementById("textSlidWrap").style.transform = `translateX(${index-200}vw)`
+        document.getElementById("textSlidWrap").style.transform = `translateX(${index-197.4}vw)`
         document.getElementById("textSlidWrap").style.transition = "0.5s"
-        changeIndex(index-200);
+        changeIndex(index-197.4);
         console.log(index)
     } 
     if(secondTextSlid.classList.contains("checked")){  
-        document.getElementById("textSlidWrap").style.transform = `translateX(${index-100}vw)`
+        document.getElementById("textSlidWrap").style.transform = `translateX(${index-98.7}vw)`
         document.getElementById("textSlidWrap").style.transition = "0.5s"
-        changeIndex(index-100);
+        changeIndex(index-98.7);
         console.log(index)
      
     }
